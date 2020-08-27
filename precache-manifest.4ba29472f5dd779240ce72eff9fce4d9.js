f7e36ffdb1276c96c9a9af05de66ff1d90069d4d
petrstepvn <petrstepvn@gmail.com>
2020-08-27 13:27:05 +0200

    Updates

---
+++ precache-manifest.4ba29472f5dd779240ce72eff9fce4d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "772d287e391a2026a03bb8cc34ad9bf9",
+    "url": "/random-option/index.html"
+  },
+  {
+    "revision": "adee44bb402f9326ca05",
+    "url": "/random-option/static/js/2.0111b000.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/random-option/static/js/2.0111b000.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45a8487ac1c7a3d123a0",
+    "url": "/random-option/static/js/main.a7aa963d.chunk.js"
+  },
+  {
+    "revision": "29c41235540c6efc42b3",
+    "url": "/random-option/static/js/runtime-main.eac81ded.js"
+  }
+]);

0e7539f26390fc6c935440c0792906e7192882a3
petrstepvn <petrstepvn@gmail.com>
2020-08-27 13:24:14 +0200

    prototype

--- src/App.js
+++ src/App.js
@@ -1,26 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import {
+	Container,
+	TextField,
+	FormControl,
+	InputLabel,
+	Input,
+	IconButton,
+	InputAdornment,
+	Button,
+	Typography
+} from '@material-ui/core';
+import List from './components/List';
+import Add from '@material-ui/icons/Add';
+import { v4 as uuidv4 } from 'uuid';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	const [Text, setText] = useState('');
+	const [Options, setOptions] = useState([]);
+	const [RandomOption, setRandomOption] = useState('');
+
+	const handleChange = (e) => {
+		setText(e.target.value);
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		if (!Text) return;
+		setOptions([...Options, { id: uuidv4(), body: Text }]);
+		setText('');
+		console.log(Options);
+	};
+
+	const handleRemove = (id) => {
+		const index = Options.map((option) => option.id).indexOf(id);
+		const newOptions = Options.map((option) => option);
+		newOptions.splice(index, 1);
+		setOptions(newOptions);
+	};
+
+	const handleRandomOption = (e) => {
+		e.preventDefault();
+		const randomIndex = Math.floor(Math.random() * Options.length);
+		setRandomOption(Options[randomIndex].body);
+	};
+
+	return (
+		<Container maxWidth="xs">
+			<form onSubmit={(e) => handleSubmit(e)}>
+				<FormControl fullWidth>
+					<InputLabel>Add new item</InputLabel>
+					<Input
+						value={Text}
+						onChange={(e) => handleChange(e)}
+						endAdornment={
+							<InputAdornment position="end">
+								<IconButton>
+									<Add />
+								</IconButton>
+							</InputAdornment>
+						}
+					/>
+				</FormControl>
+			</form>
+			<List payload={Options} handleRemove={handleRemove} />
+			<Button
+				variant="contained"
+				color="primary"
+				size="large"
+				onClick={(e) => handleRandomOption(e)}
+			>
+				Generate random option
+			</Button>
+			<Typography variant="h2">{RandomOption}</Typography>
+		</Container>
+	);
+};
 
 export default App;
---
+++ src/components/List.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import ListItemText from '@material-ui/core/ListItemText';
+import Checkbox from '@material-ui/core/Checkbox';
+import IconButton from '@material-ui/core/IconButton';
+import Delete from '@material-ui/icons/Delete';
+
+export default function CheckboxList({ payload, handleRemove }) {
+	return (
+		<List>
+			{payload.map((option) => (
+				<ListItem key={option.id} role={undefined}>
+					<ListItemText id={option.id} primary={option.body} />
+					<ListItemSecondaryAction onClick={() => handleRemove(option.id)}>
+						<IconButton edge="end" aria-label="comments">
+							<Delete color="secondary" />
+						</IconButton>
+					</ListItemSecondaryAction>
+				</ListItem>
+			))}
+		</List>
+	);
+}
